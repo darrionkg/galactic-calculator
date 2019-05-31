72cdd38d5379776fd8c4799265075dc0db9ce13d
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-05-31 09:24:05 -0700

    Add Mercury age test

--- test/solar.test.js
+++ test/solar.test.js
@@ -6,6 +6,15 @@ describe('Solar', function() {
     expect(userTest.birthDay.getFullYear()).toEqual(2014);
     expect(userTest.birthDay.getMonth()).toEqual(9);
     expect(userTest.birthDay.getDate()).toEqual(13);
+
+    it('gets Mercury age', function() {
+      let userTest = new Solar('July 11, 1995');
+      mercuryAge = userTest.getMercuryAge();
+      expect(mercuryAge.getFullYear()).toEqual(1995);
+      expect(mercuryAge.getMonth()).toEqual(6);
+      expect(mercuryAge.getDate()).toEqual(11);
   });
+
+
 
 });
