7786d8fd4203e7fe3e23da0199abeb8cdfaf6920
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-05-31 10:47:16 -0700

    Add get days alive test

--- test/solar.test.js
+++ test/solar.test.js
@@ -1,15 +1,22 @@
 import { Solar } from '../src/solar';
 
 describe('Solar', function() {
-  it('creates an instance that stores an age', function() {
-    let userTest = new Solar('October 13, 2014');
-    expect(userTest.birthDay.getFullYear()).toEqual(2014);
-    expect(userTest.birthDay.getMonth()).toEqual(9);
-    expect(userTest.birthDay.getDate()).toEqual(13);
+  let userTest;
+  beforeEach(function() {
+    userTest = new Solar('July 11, 1995');
   });
 
+  it('creates an instance that stores an age', function() {
+    expect(userTest.birthDay.getFullYear()).toEqual(1995);
+    expect(userTest.birthDay.getMonth()).toEqual(6);
+    expect(userTest.birthDay.getDate()).toEqual(11);
+  });
+
+  it('gets the total days alive', function() {
+    expect(userTest.getDaysAlive()).toEqual(8719.16);
+  })
+
   it('gets Mercury age with just year calc', function() {
-    let userTest = new Solar('July 11, 1995');
     let mercuryAge = userTest.getMercuryAge();
     let current = new Date();
     console.log(mercuryAge);
